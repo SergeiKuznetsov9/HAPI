7e71c83a30e1e6257b2a21d882beb0c2c7908dd3
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-04-21 22:43:33 +0300

    добавить простую валидацию

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 "use strict";
+
 const Hapi = require("@hapi/hapi");
+const Bcrypt = require("bcrypt");
+
+const users = {
+  john: {
+    username: "john",
+    password: "$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm", // 'secret'
+    name: "John Doe",
+    id: "2133d32a",
+  },
+};
+
+const validate = async (request, username, password) => {
+  const user = users[username];
+  if (!user) {
+    return { credentials: null, isValid: false };
+  }
+
+  const isValid = await Bcrypt.compare(password, user.password);
+  const credentials = { id: user.id, name: user.name };
+
+  return { isValid, credentials };
+};
 
 const init = async () => {
   const server = Hapi.server({
@@ -7,11 +30,28 @@ const init = async () => {
     host: "localhost",
   });
 
+  await server.register(require("@hapi/basic"));
+
+  server.auth.strategy("simple", "basic", { validate });
+
+  server.auth.default("simple");
+
   server.route({
     method: "GET",
     path: "/",
+    // options: {
+    //   auth: "simple",
+    // },
     handler: (request, h) => {
-      return "Hello World!";
+      return "You are welcome!";
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/catalogs",
+    handler: (request, h) => {
+      return "Our catalogs";
     },
   });
 
